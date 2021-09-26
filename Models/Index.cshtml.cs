67dff6d84ca69f9a4a46bb8eac038ff10114d75a
Encelad <gorba4evkirill@gmail.com>
2021-09-26 20:08:02 +0300

    Добавьте файлы проекта.

---
+++ Models/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Product_DB.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product_DB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Product> Products { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductRepository.Get_Data();
+        }
+    }
+}
---
+++ Models/Product/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Product_DB.Models.Product
+{
+    public static class ProductRepository
+    {
+        private static string conn_str = "Data Source = SQL5061.site4now.net; Initial Catalog = db_a7966a_ion; User Id = db_a7966a_ion_admin; Password=Gorb_bc24";
+
+        public static List<Product> Get_Data()
+        {
+            List<Product> products;
+            using(SqlConnection connection = new SqlConnection(conn_str))
+            {
+                connection.Open();
+                try
+                {
+                    products = connection.Query<Product>("exec GetData").ToList();
+                }
+                catch (Exception ex)
+                {
+                    products = new List<Product>(); 
+                    throw ex;
+                }
+            }
+            return products;
+        }
+
+        public static void Add_Data(Product product)
+        {
+            using (SqlConnection connection = new SqlConnection(conn_str))
+            {
+                connection.Open();
+                try
+                {
+                    connection.Query("exec AddData",product);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
